f8fc7750f86539ae59b3b2be40db008ea01c1b4b
Rafie-Ghallap <rafeia.mustafa12@gmail.com>
2025-09-10 12:36:28 +0300

    v1

---
+++ Auth-Server/router/authRouter.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const router = express.Router();
+
+let users = [{username:"user1" , password:"12345"},{username:"user2" , password:"123456"}]
+
+router.post('/login' , (req,res) => {
+    const {username , password} = req.body
+    if (!username || !password) {
+        return res.status(400).json({error : 'Username and password are required'})
+    }
+    res.json({message : 'Login successful'})
+})
+
+router.post('/register' , (req,res) => {
+    const {username , password} = req.body
+    if (!username || !password) {
+        return res.status(400).json({error : 'Username and password are required'})
+    }
+    for(let user of users){
+        if(user.username === username){
+            if(user.password === password){
+                res.status(400).json({message : 'User already exists'})
+                break;
+            }
+        }
+    }
+    res.json({message : 'Registration successful'})
+})
+
+
+
+
+
+
+
+module.exports = router
